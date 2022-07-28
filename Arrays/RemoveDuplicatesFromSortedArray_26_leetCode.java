15bf2aa929bb5726d0289e14ff9ec82815c0849d
Zakir Hussain S <s.zakirhussain02@gmail.com>
2022-07-28 22:27:23 +0530

    Update RemoveDuplicatesFromSortedArray_26_leetCode.java

--- Arrays/RemoveDuplicatesFromSortedArray_26_leetCode.java
+++ Arrays/RemoveDuplicatesFromSortedArray_26_leetCode.java
@@ -3,7 +3,7 @@ problem no: 26
 Input: nums = [0,0,1,1,1,2,2,3,3,4]
 Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores). But the input should be in given format even after removing/shifting
+It does not matter what you leave beyond the returned k (hence they are underscores). The input should be in given format even after removing/shifting
 */
 
 class Solution {
